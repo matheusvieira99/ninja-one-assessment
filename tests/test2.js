754d4077bdeb3bc39fd5f0637c4d6f853f492e14
Matheus Vieira <matheus-vieira12@hotmail.com>
2022-10-13 04:29:33 -0300

    starting test2

--- tests/test2.js
+++ tests/test2.js
@@ -1,1 +1,15 @@
 import { Selector } from "testcafe";
+import DevicesPage from '../pages/DevicesPage';
+import AddDevicePage from '../pages/AddDevicePage';
+import Logger from 'js-logger';
+Logger.useDefaults();
+
+    fixture('test2')
+        .page("http://localhost:3001");
+
+    test('should validate creation of device', async t =>{
+        await t
+            .click(DevicesPage.findAddDevice())
+            .typeText(AddDevicePage.findSystemName(), 'Ninja-PC')
+
+    });
